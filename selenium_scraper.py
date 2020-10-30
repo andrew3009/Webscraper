646147a4bd0a0740ff79dd020154d99971a1e41c
Andrew Fletcher <andrew-fletcher3009@hotmail.com>
2020-10-30 04:31:34 +0000

    test

--- selenium_scraper.py
+++ selenium_scraper.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from pyvirtualdisplay import Display
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -7,15 +8,25 @@ from selenium.webdriver.chrome.options import Options
 
 # CHROMEDRIVER_PATH = "/app/.chromedriver/bin/chromedriver"
 
-chrome_bin = os.environ.get('GOOGLE_CHROME_SHIM', None)
-options = webdriver.ChromeOptions()
-options.binary_location = chrome_bin
-options.add_argument("--disable-gpu")
-options.add_argument("--no-sandbox")
-options.add_argument('--headless')
-options.add_argument('--remote-debugging-port=9222')
-options.add_argument('window-size=1200x600')
-driver = webdriver.Chrome(executable_path="chromedriver", chrome_options=options)
+display = Display(visible=False, size=(800, 600))
+display.start()
+
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+chrome_options.add_argument('--disable-dev-shm-usage')
+chrome_options.add_argument('--no-sandbox')
+driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+
+# chrome_bin = os.environ.get('GOOGLE_CHROME_SHIM', None)
+# options = webdriver.ChromeOptions()
+# options.binary_location = chrome_bin
+# options.add_argument("--disable-gpu")
+# options.add_argument("--no-sandbox")
+# options.add_argument('--headless')
+# options.add_argument('--remote-debugging-port=9222')
+# options.add_argument('window-size=1200x600')
+# driver = webdriver.Chrome(executable_path="chromedriver", chrome_options=options)
 
 # chrome_bin = os.environ.get('GOOGLE_CHROME_SHIM', None)
 
@@ -42,10 +53,12 @@ def checkoutLoop():
     checkout = WebDriverWait(driver, 20).until(
       EC.presence_of_element_located((By.XPATH, "/html/body/div[5]/div/div[2]/div/div/a[2]"))
     )
-    
-    print('checkout')
-    
     checkout.click()
+
+    # checkoutTwo = WebDriverWait(driver, 20).until(
+    #   EC.presence_of_element_located((By.CLASS_NAME, "secure-checkout"))
+    # )
+    # checkoutTwo.click()
     print('Checkout')
     print('FINISHED')
   except:
@@ -53,6 +66,8 @@ def checkoutLoop():
 
 def loopFunc():
   try:
+    print('1')
+    
     primary = WebDriverWait(driver, 20).until(
       EC.presence_of_element_located((By.ID, "primary"))
     )
