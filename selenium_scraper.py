290d05e60a24b169761165ba426fc804204cb879
Andrew Fletcher <andrew-fletcher3009@hotmail.com>
2020-10-30 02:48:23 +0000

    New fix

--- selenium_scraper.py
+++ selenium_scraper.py
@@ -5,16 +5,16 @@ from selenium.webdriver.support import expected_conditions as EC
 import os
 from selenium.webdriver.chrome.options import Options
 
-options = Options()
+CHROMEDRIVER_PATH = "/app/.chromedriver/bin/chromedriver"
 
-options.binary_location = os.environ.get('GOOGLE_CHROME_BIN', None)
-
-options.add_argument('--headless')
-options.add_argument('--disable-gpu')
-options.add_argument('--no-sandbox')
-options.add_argument('--remote-debugging-port=9222')
-
-driver = webdriver.Chrome(executable_path=str(os.environ.get('CHROMEDRIVER_PATH', None)), chrome_options=options)
+chrome_bin = os.environ.get('GOOGLE_CHROME_BIN', "chromedriver")
+options = webdriver.ChromeOptions()
+options.binary_location = chrome_bin
+options.add_argument("--disable-gpu")
+options.add_argument("--no-sandbox")
+options.add_argument('headless')
+options.add_argument('window-size=1200x600')
+driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH, chrome_options=options)
 
 driver.get("https://www.game.co.uk/")
 
