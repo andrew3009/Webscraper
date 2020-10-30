e77761134fa40acbca298b1fe83a1205965472c1
Andrew Fletcher <andrew-fletcher3009@hotmail.com>
2020-10-30 03:03:52 +0000

    fix

--- selenium_scraper.py
+++ selenium_scraper.py
@@ -7,22 +7,23 @@ from selenium.webdriver.chrome.options import Options
 
 # CHROMEDRIVER_PATH = "/app/.chromedriver/bin/chromedriver"
 
-# chrome_bin = os.environ.get('GOOGLE_CHROME_BIN', "chromedriver")
-# options = webdriver.ChromeOptions()
-# options.binary_location = chrome_bin
-# options.add_argument("--disable-gpu")
-# options.add_argument("--no-sandbox")
-# options.add_argument('headless')
-# options.add_argument('window-size=1200x600')
-# driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH, chrome_options=options)
-
 chrome_bin = os.environ.get('GOOGLE_CHROME_SHIM', None)
+options = webdriver.ChromeOptions()
+options.binary_location = chrome_bin
+options.add_argument("--disable-gpu")
+options.add_argument("--no-sandbox")
+options.add_argument('--headless')
+options.add_argument('--remote-debugging-port=9222')
+options.add_argument("--example-flag")
+driver = webdriver.Chrome(executable_path="chromedriver", chrome_options=options)
 
-opts = Options()
+# chrome_bin = os.environ.get('GOOGLE_CHROME_SHIM', None)
 
-opts.binary_location = chrome_bin
+# opts = Options()
 
-driver = webdriver.Chrome(executable_path="chromedriver", chrome_options=opts)
+# opts.binary_location = chrome_bin
+
+# driver = webdriver.Chrome(executable_path="chromedriver", chrome_options=opts)
 
 driver.get("https://www.game.co.uk/webapp/wcs/stores/servlet/HubArticleView?langId=44&searchBtn=z&msg=&showResultsPage=true&DM_PersistentCookieCreated=true&sType=SimpleSearch&hubId=2646251&predictiveSearchURL=&resultCatEntryType=2&articleId=2646251&catalogId=10201&pageView=image&searchCount=1&searchTerm=ps5&storeId=10151&beginIndex=0&pageSize=48&ddkey=http%3AAjaxCatalogSearch")
 
