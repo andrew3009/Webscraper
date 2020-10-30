bbbe09b2f0008d6170f65819975d6193c083c39a
Andrew Fletcher <andrew-fletcher3009@hotmail.com>
2020-10-30 04:57:29 +0000

    Refreshed page instead of trying to click popup button

--- selenium_scraper.py
+++ selenium_scraper.py
@@ -1,47 +1,22 @@
+from time import sleep
 from selenium import webdriver
-from pyvirtualdisplay import Display
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import os
 from selenium.webdriver.chrome.options import Options
 
-# CHROMEDRIVER_PATH = "/app/.chromedriver/bin/chromedriver"
-
-display = Display(visible=False, size=(800, 600))
-display.start()
-
-
-chrome_options = webdriver.ChromeOptions()
-chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-chrome_options.add_argument('--disable-dev-shm-usage')
-chrome_options.add_argument('--disable-gpu')
-chrome_options.add_argument('--no-sandbox')
-driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
-
-# chrome_bin = os.environ.get('GOOGLE_CHROME_SHIM', None)
-# options = webdriver.ChromeOptions()
-# options.binary_location = chrome_bin
-# options.add_argument("--disable-gpu")
-# options.add_argument("--no-sandbox")
-# options.add_argument('--headless')
-# options.add_argument('--remote-debugging-port=9222')
-# options.add_argument('window-size=1200x600')
-# driver = webdriver.Chrome(executable_path="chromedriver", chrome_options=options)
-
-# chrome_bin = os.environ.get('GOOGLE_CHROME_SHIM', None)
-
-# opts = Options()
-
-# opts.binary_location = chrome_bin
-
-# driver = webdriver.Chrome(executable_path="chromedriver", chrome_options=opts)
+chrome_bin = os.environ.get('GOOGLE_CHROME_SHIM', None)
+options = webdriver.ChromeOptions()
+options.binary_location = chrome_bin
+options.add_argument("--disable-gpu")
+options.add_argument("--no-sandbox")
+options.add_argument('--headless')
+options.add_argument('--remote-debugging-port=9222')
+options.add_argument('window-size=1200x600')
+driver = webdriver.Chrome(executable_path="chromedriver", chrome_options=options)
 
 driver.get("https://www.game.co.uk/webapp/wcs/stores/servlet/HubArticleView?langId=44&searchBtn=z&msg=&showResultsPage=true&DM_PersistentCookieCreated=true&sType=SimpleSearch&hubId=2646251&predictiveSearchURL=&resultCatEntryType=2&articleId=2646251&catalogId=10201&pageView=image&searchCount=1&searchTerm=ps5&storeId=10151&beginIndex=0&pageSize=48&ddkey=http%3AAjaxCatalogSearch")
-
-# search = driver.find_element_by_xpath('//*[@id="search"]')
-# search.send_keys('ps5')
-# search.submit()
 
 def checkoutLoop():
   try:
@@ -50,16 +25,24 @@ def checkoutLoop():
     )
     pre.click()
     print('Added to Basket')
+
+    sleep(3)
+    driver.refresh()
     
+    print('Page Refreshed')
+
+    basket = WebDriverWait(driver, 20).until(
+      EC.presence_of_element_located((By.ID, "basketLink"))
+    )
+    basket.click()
+    
+    print('Clicked on Basket Link')
+
     checkout = WebDriverWait(driver, 20).until(
-      EC.presence_of_element_located((By.XPATH, "/html/body/div[5]/div/div[2]/div/div/a[2]"))
+      EC.presence_of_element_located((By.CLASS_NAME, "cta-large"))
     )
     checkout.click()
-
-    # checkoutTwo = WebDriverWait(driver, 20).until(
-    #   EC.presence_of_element_located((By.CLASS_NAME, "secure-checkout"))
-    # )
-    # checkoutTwo.click()
+    
     print('Checkout')
     print('FINISHED')
   except:
