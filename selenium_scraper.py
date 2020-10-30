24eaf85311dab927b85f7e9be8322e18d5d48cd4
Andrew Fletcher <andrew-fletcher3009@hotmail.com>
2020-10-30 02:59:41 +0000

    updated page

--- selenium_scraper.py
+++ selenium_scraper.py
@@ -24,11 +24,11 @@ opts.binary_location = chrome_bin
 
 driver = webdriver.Chrome(executable_path="chromedriver", chrome_options=opts)
 
-driver.get("https://www.game.co.uk/")
+driver.get("https://www.game.co.uk/webapp/wcs/stores/servlet/HubArticleView?langId=44&searchBtn=z&msg=&showResultsPage=true&DM_PersistentCookieCreated=true&sType=SimpleSearch&hubId=2646251&predictiveSearchURL=&resultCatEntryType=2&articleId=2646251&catalogId=10201&pageView=image&searchCount=1&searchTerm=ps5&storeId=10151&beginIndex=0&pageSize=48&ddkey=http%3AAjaxCatalogSearch")
 
-search = driver.find_element_by_xpath('//*[@id="search"]')
-search.send_keys('ps5')
-search.submit()
+# search = driver.find_element_by_xpath('//*[@id="search"]')
+# search.send_keys('ps5')
+# search.submit()
 
 def loopFunc():
   try:
