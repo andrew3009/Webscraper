75a58b8d759ff63167255ad06a1f7c905e999aee
Andrew Fletcher <andrew-fletcher3009@hotmail.com>
2020-10-30 02:52:57 +0000

    Bug fix 2

--- selenium_scraper.py
+++ selenium_scraper.py
@@ -5,16 +5,24 @@ from selenium.webdriver.support import expected_conditions as EC
 import os
 from selenium.webdriver.chrome.options import Options
 
-CHROMEDRIVER_PATH = "/app/.chromedriver/bin/chromedriver"
+# CHROMEDRIVER_PATH = "/app/.chromedriver/bin/chromedriver"
 
-chrome_bin = os.environ.get('GOOGLE_CHROME_BIN', "chromedriver")
-options = webdriver.ChromeOptions()
-options.binary_location = chrome_bin
-options.add_argument("--disable-gpu")
-options.add_argument("--no-sandbox")
-options.add_argument('headless')
-options.add_argument('window-size=1200x600')
-driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH, chrome_options=options)
+# chrome_bin = os.environ.get('GOOGLE_CHROME_BIN', "chromedriver")
+# options = webdriver.ChromeOptions()
+# options.binary_location = chrome_bin
+# options.add_argument("--disable-gpu")
+# options.add_argument("--no-sandbox")
+# options.add_argument('headless')
+# options.add_argument('window-size=1200x600')
+# driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH, chrome_options=options)
+
+chrome_bin = os.environ.get('GOOGLE_CHROME_SHIM', None)
+
+opts = Options()
+
+opts.binary_location = chrome_bin
+
+driver = webdriver.Chrome(executable_path="chromedriver", chrome_options=opts)
 
 driver.get("https://www.game.co.uk/")
 
