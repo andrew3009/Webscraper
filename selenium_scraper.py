71f1767cdf40684db089d5f7ba92b9e1358b53f0
Andrew Fletcher <andrew-fletcher3009@hotmail.com>
2020-10-30 02:34:20 +0000

    Get chrome working in heroku

--- selenium_scraper.py
+++ selenium_scraper.py
@@ -2,8 +2,20 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import os
+from selenium.webdriver.chrome.options import Options
 
-driver = webdriver.Chrome()
+options = Options()
+
+options.binary_location = os.environ.get('GOOGLE_CHROME_BIN')
+
+options.add_argument('--headless')
+options.add_argument('--disable-gpu')
+options.add_argument('--no-sandbox')
+options.add_argument('--remote-debugging-port=9222')
+
+driver = webdriver.Chrome(executable_path=str(os.environ.get('CHROMEDRIVER_PATH')), chrome_options=options)
+
 driver.get("https://www.game.co.uk/")
 
 search = driver.find_element_by_xpath('//*[@id="search"]')
